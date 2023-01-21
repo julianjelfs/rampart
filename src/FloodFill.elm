4686033dc431cbde57ba4539af8e1a7cc679fbd7
Julian Jelfs <julian.jelfs@gmail.com>
2023-01-21 17:10:47 +0000

    floodfill causing a stack overflow at the moment - must be something stupid

--- src/FloodFill.elm
+++ src/FloodFill.elm
@@ -1,8 +1,76 @@
 module FloodFill exposing (..)
 
-import Set exposing (Set)
 import Data exposing (Spec)
+import Set exposing (Set)
 
-findEnclosedCastles: Spec -> Set (Int, Int) -> Set (Int, Int)
+
+findEnclosedCastles : Spec -> Set ( Int, Int ) -> Set ( Int, Int )
 findEnclosedCastles spec walls =
-    spec.castles
+    let
+        visited =
+            processCell spec Set.empty walls ( -1, -1 )
+    in
+    Set.diff spec.castles visited
+
+
+processCell : Spec -> Set ( Int, Int ) -> Set ( Int, Int ) -> ( Int, Int ) -> Set ( Int, Int )
+processCell spec visited walls cell =
+    if Set.member cell visited then
+        visited
+
+    else
+        let
+            visited_ =
+                Set.insert cell visited
+        in
+        case getValidSiblings spec visited walls cell of
+            [] ->
+                visited_
+
+            xs ->
+                List.foldl
+                    (\c v ->
+                        Set.union v (processCell spec v walls c)
+                    )
+                    visited_
+                    xs
+
+
+getValidSiblings : Spec -> Set ( Int, Int ) -> Set ( Int, Int ) -> ( Int, Int ) -> List ( Int, Int )
+getValidSiblings spec visited walls cell =
+    getAdjacentCells cell |> List.filter (\c -> isInbounds spec.dimensions c && notVisited visited c && unoccupied walls c)
+
+
+isInbounds : ( Int, Int ) -> ( Int, Int ) -> Bool
+isInbounds ( cols, rows ) ( x, y ) =
+    x
+        >= -1
+        && y
+        >= -1
+        && x
+        <= cols
+        && y
+        <= rows
+
+
+notVisited : Set ( Int, Int ) -> ( Int, Int ) -> Bool
+notVisited visited cell =
+    not (Set.member cell visited)
+
+
+unoccupied : Set ( Int, Int ) -> ( Int, Int ) -> Bool
+unoccupied walls cell =
+    not (Set.member cell walls)
+
+
+getAdjacentCells : ( Int, Int ) -> List ( Int, Int )
+getAdjacentCells ( x, y ) =
+    [ ( x - 1, y - 1 )
+    , ( x, y - 1 )
+    , ( x + 1, y - 1 )
+    , ( x - 1, y )
+    , ( x + 1, y )
+    , ( x - 1, y + 1 )
+    , ( x, y + 1 )
+    , ( x + 1, y + 1 )
+    ]
--- tests/FloodFillTests.elm
+++ tests/FloodFillTests.elm
@@ -1,16 +1,120 @@
 module FloodFillTests exposing (..)
 
+import Data exposing (Spec, roundOne)
 import Expect exposing (Expectation)
+import FloodFill exposing (findEnclosedCastles, getAdjacentCells, getValidSiblings, isInbounds, notVisited)
 import Fuzz exposing (Fuzzer, int, list, string)
-import Test exposing (..)
-import TestData exposing(enclosed)
-import Data exposing (roundOne)
-import FloodFill exposing (findEnclosedCastles)
 import Set exposing (Set)
+import Test exposing (..)
+import TestData exposing (enclosed)
+
+
+testSpec : Spec
+testSpec =
+    { castles = Set.fromList [ ( 3, 3 ) ]
+    , dimensions = ( 10, 10 )
+    , ships = 5
+    }
+
+
+testWalls : Set ( Int, Int )
+testWalls =
+    Set.fromList
+        [ ( 0, 0 )
+        , ( 1, 0 )
+        , ( 2, 0 )
+        , ( 3, 0 )
+        , ( 4, 0 )
+        , ( 5, 0 )
+        , ( 5, 1 )
+        , ( 5, 2 )
+        , ( 5, 3 )
+        , ( 5, 4 )
+        , ( 5, 5 )
+        , ( 4, 5 )
+        , ( 3, 5 )
+        , ( 2, 5 )
+        , ( 1, 5 )
+        , ( 0, 5 )
+        , ( 0, 4 )
+        , ( 0, 3 )
+        , ( 0, 2 )
+        , ( 0, 1 )
+        ]
+
 
 floodFill : Test
 floodFill =
     describe "The floodfill algorithm"
         [ test "finds one enclosed castle at (3,8)" <|
-            \_ -> Expect.equal (Set.fromList [(3,8)]) (findEnclosedCastles roundOne enclosed ) 
+            \_ -> Expect.equal (Set.fromList [ ( 3, 8 ) ]) (findEnclosedCastles roundOne enclosed)
+        , test "finds no enclosed castles" <|
+            \_ -> Expect.equal Set.empty (findEnclosedCastles testSpec Set.empty)
+        , test "finds enclosed castle at (3,3)" <|
+            \_ -> Expect.equal (Set.fromList [ ( 3, 3 ) ]) (findEnclosedCastles testSpec testWalls)
+        , test "get adjacent cells" <|
+            \_ ->
+                Expect.equal
+                    [ ( -2, -2 )
+                    , ( -1, -2 )
+                    , ( 0, -2 )
+                    , ( -2, -1 )
+                    , ( 0, -1 )
+                    , ( -2, 0 )
+                    , ( -1, 0 )
+                    , ( 0, 0 )
+                    ]
+                    (getAdjacentCells ( -1, -1 ))
+        , test "is inbounds one" <|
+            \_ ->
+                Expect.equal
+                    True
+                    (isInbounds ( 10, 10 ) ( 5, 5 ))
+        , test "is inbounds two" <|
+            \_ ->
+                Expect.equal
+                    True
+                    (isInbounds ( 10, 10 ) ( -1, -1 ))
+        , test "is inbounds three" <|
+            \_ ->
+                Expect.equal
+                    True
+                    (isInbounds ( 10, 10 ) ( 10, 10 ))
+        , test "is out of bounds one" <|
+            \_ ->
+                Expect.equal
+                    False
+                    (isInbounds ( 10, 10 ) ( -2, 5 ))
+        , test "is out of bounds two" <|
+            \_ ->
+                Expect.equal
+                    False
+                    (isInbounds ( 10, 10 ) ( 11, 5 ))
+        , test "is not visited" <|
+            \_ ->
+                Expect.equal
+                    True
+                    (notVisited (Set.fromList [ ( 0, 0 ), ( 5, 5 ) ]) ( 6, 7 ))
+        , test "is visited" <|
+            \_ ->
+                Expect.equal
+                    False
+                    (notVisited (Set.fromList [ ( 0, 0 ), ( 5, 5 ) ]) ( 5, 5 ))
+        , describe "valid siblings"
+            [ test "empty grid - first iteration" <|
+                \_ ->
+                    Expect.equal
+                        [ ( 0, -1 ), ( -1, 0 ), ( 0, 0 ) ]
+                        (getValidSiblings testSpec Set.empty Set.empty ( -1, -1 ))
+            , test "excludes visited" <|
+                \_ ->
+                    Expect.equal
+                        [ ( 0, -1 ), ( 0, 0 ) ]
+                        (getValidSiblings testSpec (Set.fromList [ ( -1, 0 ) ]) Set.empty ( -1, -1 ))
+            , test "excludes visited and walls" <|
+                \_ ->
+                    Expect.equal
+                        [ ( 0, -1 ) ]
+                        (getValidSiblings testSpec (Set.fromList [ ( -1, 0 ) ]) (Set.fromList [ ( 0, 0 ) ]) ( -1, -1 ))
+            ]
         ]
