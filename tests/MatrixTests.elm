ffda03d3df093f8a0f064f631d5bd876f98ca93a
Julian Jelfs <julian.jelfs@gmail.com>
2023-01-22 17:50:02 +0000

    made a start on the rotation

---
+++ tests/MatrixTests.elm
@@ -0,0 +1,74 @@
+module MatrixTests exposing (..)
+
+import Data exposing (Point, Spec, roundOne)
+import Expect exposing (Expectation)
+import Matrix exposing (Matrix)
+import Test exposing (..)
+
+
+sshape =
+    Matrix.fromLists
+        [ [ 0, 0, 1, 1, 0 ]
+        , [ 0, 0, 1, 0, 0 ]
+        , [ 0, 0, 1, 0, 0 ]
+        , [ 0, 0, 1, 0, 0 ]
+        , [ 0, 1, 1, 0, 0 ]
+        ]
+
+
+sshapeRotated =
+    Matrix.fromLists
+        [ [ 0, 0, 0, 0, 0 ]
+        , [ 1, 0, 0, 0, 0 ]
+        , [ 1, 1, 1, 1, 1 ]
+        , [ 0, 0, 0, 0, 1 ]
+        , [ 0, 0, 0, 0, 0 ]
+        ]
+
+
+lshape =
+    Matrix.fromLists
+        [ [ 0, 1, 1 ]
+        , [ 0, 1, 0 ]
+        , [ 0, 1, 0 ]
+        ]
+
+
+lshapeRotated =
+    Matrix.fromLists
+        [ [ 0, 0, 0 ]
+        , [ 1, 1, 1 ]
+        , [ 0, 0, 1 ]
+        ]
+
+
+pretty =
+    Matrix.pretty String.fromInt
+
+
+flippedIdentity =
+    Matrix.identity >> Matrix.toLists >> List.map List.reverse >> Matrix.fromLists
+
+
+testRotation name dim before after =
+    test (name ++ " rotate 90%") <|
+        \_ ->
+            Maybe.map2 (\m i -> ( m, Matrix.transpose m, i )) before (flippedIdentity dim)
+                |> Maybe.andThen
+                    (\( m, t, i ) ->
+                        Matrix.dot t i
+                    )
+                |> Maybe.map2
+                    (\r res ->
+                        Expect.equal r res
+                    )
+                    after
+                |> Maybe.withDefault (Expect.fail ("didn't get the right rotation for " ++ name))
+
+
+matrixTests : Test
+matrixTests =
+    describe "Playing with matrix rotations"
+        [ testRotation "lshape" 3 lshape lshapeRotated
+        , testRotation "sshape" 5 sshape sshapeRotated
+        ]
